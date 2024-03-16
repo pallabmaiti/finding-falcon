73020255151c18b3272bff0221bc7b75c96bcfbb
Pallab Maiti <pallabmaiti@Pallabs-MacBook-Pro-M1.local>
2024-03-16 15:22:28 +0530

    test cases for views

--- FindingFalconTests/FindFalcon/FindFalconViewTests.swift
+++ FindingFalconTests/FindFalcon/FindFalconViewTests.swift
@@ -6,9 +6,18 @@
 //
 
 import XCTest
+import ViewInspector
 @testable import FindingFalcon
 
 final class FindFalconViewTests: XCTestCase {
+    
+    @MainActor func test_ProgressView() throws {
+        var view = FindFalconView()
+        let progressViewText = try view.inspect().navigationView().form().section(0).progressView(0).labelView().text().string()
+
+        XCTAssertEqual(progressViewText, "Loading...")
+    }
+    
     @MainActor func test_retrieveToken() async {
         var view = FindFalconView()
         
